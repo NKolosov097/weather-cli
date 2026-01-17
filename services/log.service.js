228afdcc40ebad827c98aabed06b046903a33e5a
Kolosov Nikita <N.Kolosov2003@mail.ru>
2026-01-17 08:23:19 +0300

    refactor: updated exports from log service

--- services/log.service.js
+++ services/log.service.js
@@ -1,19 +1,17 @@
 import chalk from "chalk"
 
-const printError = (error) => {
+export const printError = (error) => {
   console.log(chalk.bgRed(" ERROR "), error)
 }
 
-const printSuccess = (message) => {
+export const printSuccess = (message) => {
   console.log(chalk.bgGreen(" SUCCESS "), message)
 }
 
-const printHelp = () => {
+export const printHelp = () => {
   console.log(chalk.bgCyan(" HELP "), "Available commands:")
   console.log("\t -h for getting help")
   console.log("\t -c [CITY] for setting city")
   console.log("\t -t [TOKEN] for token")
   console.log("\t  without params for getting the weather")
 }
-
-export { printError, printSuccess, printHelp }
