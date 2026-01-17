69d851678e3a9c7169a37dc78baa8e5d62ca5292
Kolosov Nikita <N.Kolosov2003@mail.ru>
2026-01-17 16:39:35 +0300

    feat(log service): added getIcon and print weather helpers

--- services/log.service.js
+++ services/log.service.js
@@ -1,5 +1,28 @@
 import chalk from "chalk"
 
+const getIcon = (icon) => {
+  switch (icon.slice(0, -1)) {
+    case "01":
+      return "☀️"
+    case "02":
+      return "🌤️"
+    case "03":
+      return "☁️"
+    case "04":
+      return "☁️"
+    case "09":
+      return "🌧️"
+    case "10":
+      return "🌦️"
+    case "11":
+      return "🌩️"
+    case "13":
+      return "❄️"
+    case "50":
+      return "🌫️"
+  }
+}
+
 export const printError = (error) => {
   console.log(chalk.bgRed(" ERROR "), error)
 }
@@ -14,4 +37,15 @@ export const printHelp = () => {
   console.log("\t -c [CITY] for setting city")
   console.log("\t -t [TOKEN] for token")
   console.log("\t  without params for getting the weather")
+}
+
+export const printWeather = (weather) => {
+  console.log(
+    chalk.bgBlue(" WEATHER "),
+    `\n\tCity: ${weather.name}`,
+    `\n\t${getIcon(weather.weather[0].icon)}  ${weather.weather[0].description}`,
+    `\n\tTemperature: ${weather.main.temp}°C (feels like ${weather.main.feels_like}°C)`,
+    `\n\tWind: ${weather.wind.speed} m/s`,
+    `\n\tHumidity: ${weather.main.humidity}%`,
+  )
 }
